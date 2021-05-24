6fd302dd438c6cd4209427baae9b66e287ea93f1
Dani Lucaci <danilucaci@gmail.com>
2021-05-24 18:19:03 +0200

    feat: working mikro orm post create

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-let a: string = "hola-tres";
+import { MikroORM } from "@mikro-orm/core";
 
-console.log(a);
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+(async () => {
+  try {
+    const orm = await MikroORM.init(microConfig);
+
+    // run the migrations before all
+    await orm.getMigrator().up();
+
+    // const post = orm.em.create(Post, { title: "my first post" });
+    // await orm.em.persistAndFlush(post);
+
+    const posts = await orm.em.find(Post, {});
+
+    console.log(posts);
+  } catch (error) {
+    console.log(error);
+  }
+})();
